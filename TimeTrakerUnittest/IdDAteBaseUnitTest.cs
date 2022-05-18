348a30ed80f1a060b8805d2937934f0ec9d8e5df
Daniil <git config --global user.email>
2022-05-18 13:00:39 +0300

    df

--- TimeTrakerUnittest/IdDAteBaseUnitTest.cs
+++ TimeTrakerUnittest/IdDAteBaseUnitTest.cs
@@ -9,18 +9,21 @@ namespace TimeTrakerUnittest
     public class IdDAteBaseUnitTest
     {
         [TestMethod]
-        public void GetNameTryId()
+        public void DateStartStringTest()
         {
             // arange
-
-
+            
+            TasksOver tasksOver = new TasksOver(40, "ДаваймногоДней", "ага", new DateTime(2022, 5, 15, 1, 1, 58), new DateTime(2022, 5, 17, 1, 1, 58));
+            string resultEX = "15/5/2022";
             // act
 
-           
+            string relust = tasksOver.DateStartString();
 
             // assert
-            
 
+            Assert.AreEqual(resultEX, relust);
         }
+
+
     }
 }
